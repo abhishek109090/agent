42158bc505930ac941cdf621bcd0450f502f216e
abhishek109090 <152470844+abhishek109090@users.noreply.github.com>
2023-12-20 11:47:42 +0530

    Update VerifiedTruck.js

--- src/VerifiedTruck.js
+++ src/VerifiedTruck.js
@@ -12,7 +12,7 @@ function VerifiedTruck() {
 console.log('crn,',feildcrn)
   useEffect(() => {
     // Fetch owner data based on CRN
-    axios.get(`http://3.109.145.125:9001/verified?status=Completed&feildcrn=${feildcrn}`)
+    axios.get(`http://65.0.50.168:9001/verified?status=Completed&feildcrn=${feildcrn}`)
       .then((response) => {
         setVerifiedTrucks(response.data);
         console.log(response.data)
